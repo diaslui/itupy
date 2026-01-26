7ab4b52e41df03b4f4aa4b107d35a275ef800fe8
Pedro Luis Dias <luisp.diias@gmail.com>
2026-01-26 17:25:27 -0300

    feat: add view engine/services routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+import pagesRoutes from './services/pages/routes.ts';
+import { fileURLToPath } from "url";
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __rootdir = path.join(__dirname, "..");
+process.loadEnvFile(path.join(__rootdir, '.env'));
 
 const app = express();
+app.use("/assets", express.static(path.join(__rootdir, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__rootdir, "src", "views"));
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`running on http://localhost:${PORT}`);
-});
+app.use('/', pagesRoutes);
+
+
+if (process.env.NODE_ENV !== 'production'){
+  app.listen(3000, () => {
+    console.log(`running on http://localhost:3000`);
+  });
+}
